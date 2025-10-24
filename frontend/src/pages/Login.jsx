41f5a8b59270760e8833b389e2a617a83d740fab
afiyanaaj7482 <afiyanaaj7482@gmail.com>
2025-10-24 15:24:18 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import Logo from "../assets/logo.png";
 import google from "../assets/google.png";
 import { useNavigate } from "react-router-dom";
 import { IoEyeOutline, IoEye } from "react-icons/io5";
-import { authDataContext } from "./context/authContext";
+import { authDataContext } from "./context/AuthContext";
 import axios from "axios";
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../../utils/Firebase";
