58497315c890a0d39c99005acd67caebf9c5516f
afiyanaaj7482 <afiyanaaj7482@gmail.com>
2025-10-27 13:23:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://ecommerce-frontendd-vkfo.onrender.com" , "http://localhost:5174"],
  credentials:true
 }))
 
